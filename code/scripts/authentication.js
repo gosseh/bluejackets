021004918d9aa9c71f00bd495225b8e60632cdd4
shahshriya <shriyash@umich.edu>
2020-11-26 17:30:40 -0500

    finished authentication - option for register, log in, and reset password

--- code/scripts/authentication.js
+++ code/scripts/authentication.js
@@ -1,37 +1,42 @@
-////  Global Variables   ////
-var nameInput;
-var usernameInput;
-var emailInput;
-var passwordInput;
-var registerBtn;
-var signInBtn;
-
-
-
 ////  Functional Code  ////
 
 // Main
 $(document).ready(function () {
-  nameInput = $("#nameIn");
-  usernameInput = $("#usernameIn");
-  emailInput = $("#emailIn");
-  passwordInput = $("#passwordIn");
-  registerBtn = $("#registerButton");
-  signInBtn = $(".signin");
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      window.location.href = './index.html';
+    }
+  });
+  
+  let nameInput = $("#nameIn");
+  let emailInput = $("#emailIn");
+  let passwordInput = $("#passwordIn");
+  let registerBtn = $("#registerButton");
+  let signInBtn = $(".signin");
 
+  //registration
   $(registerBtn).click(function () {
-    var email = emailInput.val();
-    var password = passwordInput.val();
+    let email = emailInput.val();
+    let password = passwordInput.val();
+    let name = nameInput.val();
     if(!email.includes("@umich.edu"))
     {
       alert("Email must be a umich email");
+    }
+    else if(!name.length > 0)
+    {
+      alert("Please enter your name");
+    }
+    else if(!(password.length >= 6))
+    {
+      alert("Password must be at least 6 characters");
     }
     else
     {
       firebase.auth().createUserWithEmailAndPassword(email, password)
       .then((user) => {
-        var name = nameInput.val();
-        var uid = firebase.auth().currentUser.uid;
+        let name = nameInput.val();
+        let uid = firebase.auth().currentUser.uid;
         console.log(uid);
           firebase.database().ref('users/' + uid).set({
             username: name,
@@ -47,15 +52,56 @@ $(document).ready(function () {
         
       })
       .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        alert("This email already has an account");
+        let errorCode = error.code;
+        let errorMessage = error.message;
         console.log(errorCode);
         console.log(errorMessage);
       });
-    }
-    
+    }    
   });
 
+  //already have an account - sign in
+  $(signInBtn).click(function () {
+    window.location.href = './login.html';
+  });
+
+  let emailIn = $("#emailExists");
+  let passwordIn = $("#passwordExists");
+  let signInBtn2 = $("#signInButton");
+  let passwordBtn = $(".forgotPassword");
+
+  $(signInBtn2).click(function () {
+    let email = emailIn.val();
+    let password = passwordIn.val();
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      window.location.href = './index.html';
+    })
+    .catch((error) => {
+      alert("Username or Password is Incorrect");
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+  });
+
+  $(passwordBtn).click(function () {
+    let email = emailIn.val();
+    if(!email.includes("@umich.edu"))
+    {
+      alert("Please enter a valid email");
+    }
+    else{
+      let auth = firebase.auth();
+      auth.sendPasswordResetEmail(email).then(function() {
+        alert("Please check your email for a password reset link");
+      }).catch(function(error) {
+        alert("Email not found");
+      });
+    }
+  });
 
   
 
