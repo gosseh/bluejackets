9a0543169c958e12e886b328e70e554979f64199
shahshriya <shriyash@umich.edu>
2020-12-04 22:13:10 -0500

    adjusted small details

--- code/scripts/main.js
+++ code/scripts/main.js
@@ -109,8 +109,6 @@ $(document).ready(function () {
                     }
                     count2++;
                   }
-                  document.body.scrollTop = document.documentElement.scrollTop = 0;
-                  $('body').addClass('stop-scrolling');
                 });
 
                 $(backBtn).click(function () {
@@ -338,7 +336,7 @@ function get_distance(lat1, lon1, lat2, lon2) {
 }
 
 function createPostDiv(url, index, time, name, capacity, sport, people, distance) {
-  return "<div class='card border-dark mr-5 mb-2' style='width: 23em'id=post" + index + "'><img class='card-img-top' src='" + url + " alt='user-image'><div class='card-body text-center'><h4 class='card-title'>" + name + " - " + sport + "</h4><p class='card-text'>Capacity: " + people + "/" + capacity + "</p><p class='user-time'>Posted: " + time + "</p><p class='user-time'>" + distance + "</p><button class='btn btn-secondary user-info-btn' id='user-info" + index + "'> More Info </button> <button class='btn btn-secondary user-add-btn' id='user-add" + index + "'> Add </button></div></div>";
+  return "<div class='card border-dark mx-auto mr-5 mb-2 mt-5' style='width: 23em;'id=post" + index + "'><img class='card-img-top' src='" + url + " alt='user-image'><div class='card-body text-center'><h4 class='card-title'>" + name + " - " + sport + "</h4><p class='card-text'>Capacity: " + people + "/" + capacity + "</p><p class='user-time'>Posted: " + time + "</p><p class='user-time'>" + distance + "</p><button class='btn btn-secondary user-info-btn' id='user-info" + index + "'> More Info </button> <button class='btn btn-secondary user-add-btn' id='user-add" + index + "'> Add </button></div></div>";
 }
 
 function createExpandedDiv(index, location, sport, name, url) {
